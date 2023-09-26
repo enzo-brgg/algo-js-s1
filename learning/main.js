5791b65ce1844b4aff97842c1abe266b80cb1fe5
enzo-brgg <enzobourguignon3@gmail.com>
2023-09-26 17:37:02 +0200

    Coding Learning Exercice 1-2
    Cours du 26/09/2023

---
+++ learning/main.js
@@ -0,0 +1,13 @@
+//alert("Bonjour, êtes vous sûr de quitter cette page ?")
+
+//console.log("Bienvenue à Poudlard")
+
+const teacherage = 28
+const studentage = 19
+
+if(teacherage > studentage) {
+    console.log ("Test")
+}
+else {
+    console.log ("test2")
+}
